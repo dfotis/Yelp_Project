ae22be4e3618324609c2c7df180e1caf04d2534c
dfotis <fotis.dil94@gmail.com>
2018-05-08 01:16:23 +0300

    Initial Commit

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+#test
 import pymongo
 
+def find_restaurants_by_neighborhood(neighborhood):
+    return db['restaurants'].find({'neighborhood': neighborhood})
+
+def find_reviews_that_contain_a_word(word):
+    return db['reviews'].find({'text': {'$regex': '.*' + word + '.*'}})
+
 client = pymongo.MongoClient('localhost', 27017)
-db = client['yelp']
+db = client['yelpdb']
+
+#eastside_restaurants = find_restaurants_by_neighborhood("Eastside")
+#for restaurant in eastside_restaurants:
+#    print(restaurant)
+
+
+#match_reviews = find_reviews_that_contain_a_word("awesome")
+#for reviews in match_reviews:
+#    print(reviews)
